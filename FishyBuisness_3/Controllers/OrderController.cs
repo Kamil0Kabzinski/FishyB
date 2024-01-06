6f9726b9c1815412707d0cc1e8b9e07bf095774b
KacperKabzinski <kackab056@gmail.com>
2024-01-06 16:26:28 +0100

    Order

---
+++ FishyBuisness_3/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using FishyBuisness_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+public class OrderController : Controller
+{
+    public IActionResult Index()
+    {
+        // Pobierz dane zamówień (przykładowe dane)
+        var orders = GetOrdersFromDatabaseOrOtherSource();
+
+        return View(orders);
+    }
+
+    private List<Order> GetOrdersFromDatabaseOrOtherSource()
+    {
+       
+        return new List<Order>
+        {
+           
+            
+        };
+    }
+
+    public IActionResult AddToCart(int fishId)
+    {
+        
+        return RedirectToAction("Index");
+    }
+}
---
+++ FishyBuisness_3/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FishyBuisness_3.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string FishName { get; set; }
+        public DateTime OrderDate { get; set; }
+        
+    }
+}
